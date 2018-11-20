d3e15b28c43c19c14a8fc3acaff3271ceb02e841
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-11-20 15:48:44 -0700

    second api for dui arrests and mild styling

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.getElementById("getData").addEventListener("click", getData);
+document.getElementById("getDui").addEventListener("click", getDui);
 
 function getData() {
   fetch("http://nflarrest.com/api/v1/crime")
@@ -16,3 +17,21 @@ function getData() {
       document.getElementById("output").innerHTML = output;
     });
 }
+
+function getDui() {
+  fetch("http://nflarrest.com/api/v1/crime/topPlayers/DUI")
+    .then(res => res.json())
+    .then(data => {
+      let output2 = "<h2>DUI List</h2>";
+      data.forEach(function(DUI) {
+        output2 += `
+        <ul>
+        <li>Name: ${DUI.Name} </li>
+        <li>Number of Arrests: ${DUI.arrest_count} </li>
+        </ul>
+        `;
+      });
+      document.getElementById("output2").innerHTML = output2;
+    });
+}
+
